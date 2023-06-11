244ddf4534581677034cb941ff89976a934cb1d7
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2023-06-11 17:14:33 +0530

    Lecture 30 added

---
+++ Lecture30 Macros, Global Variables, Inline Functions & Default Args/macros.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+#define PI 3.14
+
+int main(){
+    int r = 5;
+    // int pi = 3.14;
+    double area = PI * r * r;
+    
+    cout<< "Area: "<< area << endl;
+    
+    return 0;
+}
